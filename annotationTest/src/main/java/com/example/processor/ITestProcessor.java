aa072e4b43547de260c5597ecbcfdb0716242e28
e <2572679440@qq.com>
2020-11-15 14:03:27 +0800

    init

---
+++ annotationTest/src/main/java/com/example/processor/ITestProcessor.java
@@ -0,0 +1,4 @@
+package com.example.processor;
+
+public interface ITestProcessor {
+}
---
+++ annotationTest/src/main/java/com/example/test/TestProcessor.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+
+import com.example.processor.InterfaceAnnotation;
+
+/**
+ * TODO 类描述信息
+ *
+ * @author lzk1012
+ * @version 1.0
+ * @date 2020/11/15 00:02
+ */
+@InterfaceAnnotation
+public class TestProcessor {
+}
